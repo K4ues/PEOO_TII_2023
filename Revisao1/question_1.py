490fa60a52300cc787c4054639b3fbdaa6ae196f
K4ues <124373847+K4ues@users.noreply.github.com>
2023-04-14 10:15:52 -0300

    Create question_1.py
    This code checks the greatest number between two integer values, if they're equal will show the message: "Números iguais"

---
+++ Revisao1/question_1.py
@@ -0,0 +1,8 @@
+number1 = int(input())
+number2 = int(input())
+if number1 > number2:
+  print(f'{number1})
+elif number2 > number1:
+        print(f'{number2}')
+else:
+        print('Números iguais')

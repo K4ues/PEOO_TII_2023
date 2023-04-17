0da33f216bea94a083ea5cf1090749a99d017536
K4ues <124373847+K4ues@users.noreply.github.com>
2023-04-17 10:58:28 -0300

    Delete question_1.py

--- Revisao1/question_1.py
+++
@@ -1,8 +0,0 @@
-number1 = int(input())
-number2 = int(input())
-if number1 > number2:
-  print(f'{number1}')
-elif number2 > number1:
-        print(f'{number2}')
-else:
-        print('Números iguais')

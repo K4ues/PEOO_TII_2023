fd5ceaf60c86202895a1e34d9636e2376484df79
K4ues <124373847+K4ues@users.noreply.github.com>
2023-05-03 16:00:06 -0300

    Create Question_5.py

---
+++ Revisão_1/Question_5.py
@@ -0,0 +1,25 @@
+month = input()
+if month == 1:
+  print('O mês de janeiro é do primeiro trimestre do ano')
+elif month == 2:
+  print('O mês de fevereiro é do primeiro trimestre do ano')
+elif month == 3:
+  print('O mês de março é do primeiro trimestre do ano')
+elif month == 4:
+  print('O mês de abril é do segundo trimestre do ano')
+elif month == 5:
+  print('O mês de maio é do segundo trimestre do ano')
+elif month == 6:
+  print('O mês de junho é do segundo trimestre do ano')
+elif month == 7:
+  print('O mês de julho é do terceiro trimestre do ano')
+elif month == 8:
+  print('O mês de agosto é do terceiro trimestre do ano')
+elif month == 9:
+  print('O mês de setembro é do terceiro trimestre do ano')
+elif month == 10:
+  print('O mês de outubro é do quarto trimestre do ano')
+elif month == 11:
+  print('O mês de novembro é do quarto trimestre do ano')
+elif month == 12:
+  print('O mês de dezembro é do quarto trimestre do ano ')
